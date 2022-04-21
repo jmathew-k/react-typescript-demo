ffcdc3780591445bb0a6fbb01468b0c297c9064f
Joshua Mathew <JoshuaKMathew@gmail.com>
2022-04-21 17:04:24 +0530

    lesson 4

--- src/components/Greet.tsx
+++ src/components/Greet.tsx
@@ -8,7 +8,9 @@ export const Greet = (props: GreetProps) => {
 	return (
 		<div>
 			<h2>
-				props.isLoggedIn ? `Welcome ${props.name}! You have ${props.messageCount}`
+				{props.isLoggedIn
+					? `Welcome ${props.name}! You have ${props.messageCount} unread messages`
+					: 'Welcome Guest'}
 			</h2>
 		</div>
 	)
